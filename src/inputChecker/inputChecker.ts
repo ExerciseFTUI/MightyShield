958ca884757dc1c199a42acd1fa488b8ea26d7a0
MorpKnight <christoffelsihombing@gmail.com>
2023-09-14 21:37:26 +0700

    fix: inputChecker class problem

--- src/inputChecker/inputChecker.ts
+++ src/inputChecker/inputChecker.ts
@@ -22,55 +22,64 @@ export class defendInput{
 }
 
 export class defendMiddleware {
-    public static checkBody = false;
-    public static checkParams = false;
+    public static checkBody: boolean;
+    public static checkParams: boolean;
 
-    constructor(...args: {checkBody?: boolean, checkParams?: boolean}[]){
+    constructor(...args: {checkBody: boolean, checkParams: boolean}[]){
         defendMiddleware.checkBody = args[0]?.checkBody ?? false;
         defendMiddleware.checkParams = args[0]?.checkParams ?? false;
     }
 
-        protect(){
-            return (req:Request, res:Response, next:NextFunction) => {
-                if(defendMiddleware.checkBody){
-                    const result = this.checkSQLandXSS(req, res, next);
-                    if(result){
-                        return res.status(400).send("Bad Request");
-                    }
+    protect(){
+        return (req:Request, res:Response, next:NextFunction) => {
+            let check: boolean = false;
+            if(defendMiddleware.checkBody){
+                const result = this.checkSQLandXSS(req, res, next);
+                if(result){
+                    check = true;
                 }
-                if(defendMiddleware.checkParams){
-                    const result = this.checkParameter(req, res, next);
-                    if(result){
-                        return res.status(400).send("Bad Request");
-                    }
+            }
+            if(defendMiddleware.checkParams){
+                const result = this.checkParameter(req, res, next);
+                if(result){
+                    check = true;
                 }
-                next();
+            }
+
+            if(check){
+                res.status(400).json({
+                    status: 'SQL Injection or XSS attack detected',
+                    message: 'Bad request'
+                })
+            }
+
+            next();
+        }
+    }
+
+    private checkSQLandXSS(req: Request, res: Response, next: NextFunction) {
+        const data = req.body;
+
+        for(const key in data){
+            const val = data[key];
+            const result = new defendInput(val).checkAll();
+            
+            if(result){
+                return true;
             }
         }
+    }
 
-        private checkSQLandXSS(req: Request, res: Response, next: NextFunction) {
-            const data = req.body;
+    private checkParameter(req: Request, res: Response, next: NextFunction){
+        const data = req.params;
 
-            for(const key in data){
-                const val = data[key];
-                const result = new defendInput(val).checkAll();
-                
-                if(result){
-                    return true;
-                }
+        for(const key in data){
+            const val = data[key];
+            const devodeVal = decodeURIComponent(val as string);
+            const result = new defendInput(devodeVal).checkAll();
+            if(result){
+                return true;
             }
         }
-
-        private checkParameter(req: Request, res: Response, next: NextFunction){
-            const data = req.params;
-
-            for(const key in data){
-                const val = data[key];
-                const devodeVal = decodeURIComponent(val as string);
-                const result = new defendInput(devodeVal).checkAll();
-                if(result){
-                    return true;
-                }
-            }
-        }    }
-
+    }
+}
