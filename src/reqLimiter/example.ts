2c709ebd09ec31714476756a2cf9d4ccdbc42e85
kamalMakarim <kamal.makarim@gmail.com>
2023-09-23 17:19:31 +0700

    parameter optional, comments, utils

---
+++ src/reqLimiter/example.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import requestLimiter from './reqLimiter.js';
+import requestLimiterJson from './requestLimiterJson.js';
+
+const app = express();
+const port = 3000;
+
+app.use('/login', requestLimiter({resetTime: 60000, maxRequests: 10, cleanupInterval: 60000})); 
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/login', (request, response) => {
+  response.send('Access granted');
+});
+
+app.post('/login', (request, response) => {
+  response.send('Access granted');
+});
--- src/reqLimiter/reqLimiter.ts
+++ src/reqLimiter/reqLimiter.ts
@@ -1,84 +1,71 @@
 import { Request, Response, NextFunction } from 'express';
 import requestIp from 'request-ip';
+import { cleanupRequestCount, getClientIp } from './utils.js';
 
 interface RequestCount {
-  [key: string]: {
-    timestamp: number;
-    count: number;
+  [key: string]: {      //the IP address
+    timestamp: number;  //When the ip starts to send request
+    count: number;      //The amount of request sent
   };
 }
 
-function cleanupRequestCount(requestCount: RequestCount, resetTime: number) {
-  const currentTime = Date.now();
-
-  for (const clientIP in requestCount) {
-    if (currentTime - requestCount[clientIP].timestamp >= resetTime) {
-      delete requestCount[clientIP];
-    }
-  }
-}
-
-function getClientIp(req: Request, res: Response): string | null {
-  try {
-    const clientIp: string | null = requestIp.getClientIp(req);
-
-    // Validate the IP address (IPv4 or IPv6)
-    const ipPattern = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$|^[0-9a-fA-F:]+:[0-9a-fA-F]*:[0-9a-fA-F]$/;
-
-    if (clientIp && ipPattern.test(clientIp)) {
-      return clientIp;
-    } else {
-      res.status(400).send('Invalid client IP address');
-      return null;
-    }
-  } catch (error) {
-    console.error('Error in getClientIp:', error);
-    res.status(500).send('Internal Server Error');
-    return null;
-  }
-}
-
-export default function requestLimiter(maxRequests: number = 100, resetTime: number = 300000, cleanupInterval: number = 30000) {
+export default function requestLimiter({
+  maxRequests = 100,            //default value of maxRequest
+  resetTime = 300000,           //default value of resetTime in ms (5 mins)
+  cleanupInterval = 300000,     //default value of cleanupInterval in ms (5 mins)
+  }:{
+    maxRequests? : number;      //max amount of time a request could be sent before getting blocked
+    resetTime? : number;        //The time to allow reset of count on a certain ip
+    cleanupInterval? : number;  //The interval to cleanup no longger used data on requestCount
+  }) {
   const requestCount: RequestCount = {};
 
-  setInterval(() => {
-    try {
-      cleanupRequestCount(requestCount, resetTime);
-    } catch (error) {
-      console.error('Error during cleanup:', error);
-    }
-  }, cleanupInterval);
+  //Clean any data that has pass the resetTime for every cleanupInterval
+  cleanupRequestCount(requestCount, resetTime, cleanupInterval);
 
   return function (req: Request, res: Response, next: NextFunction) {
-    const clientIP = getClientIp(req, res);
-    const currentTime = Date.now();
+    const clientIP = getClientIp(req, res); 
+    const currentTime = Date.now();         
 
     try {
+      //if clientIP doesn't exist in requestCount, initiate it
       if (!requestCount[clientIP]) {
         requestCount[clientIP] = {
           timestamp: currentTime,
           count: 1,
         };
-      } else {
+      } 
+      
+      //if clientIP exist in requestCount
+      else {
         // Check if the reset time has passed, and if so, reset the count and timestamp
         if (currentTime - requestCount[clientIP].timestamp >= resetTime) {
           requestCount[clientIP] = {
             timestamp: currentTime,
             count: 1,
           };
-        } else {
+        } 
+        
+        //adds the count for an existing clientIP in requestCount
+        else {
           requestCount[clientIP].count++;
         }
       }
 
+      //if the count is less than or equal to maxRequest
       if (requestCount[clientIP].count <= maxRequests) {
         next();
-      } else {
+      } 
+      
+      //if the count is more than maxRequest
+      else {
         res.status(429).send('Too many requests (Max requests reached)');
       }
+
     } catch (error) {
       console.error('Error in requestLimiter middleware:', error);
       res.status(500).send('Internal Server Error');
     }
   };
 }
+
